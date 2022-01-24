8b2047cfecb609271b32d559df68c4f84497d533
Benjamin Ogbonna <32393357+benjaminogbonna@users.noreply.github.com>
2022-01-24 21:36:33 +0100

    Update utils.py

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,9 +1,8 @@
 import os
 from twilio.rest import Client
 
-account_sid = 'AC7d9582455b737bb1259b5639885ddce7'
-# messaging_sid = 'MG480035d91e2677025d7bc9e569922819'
-auth_token = '20c59b4fd2306fea7d75e62e328cf2ee'
+account_sid = ''  # change with yours
+auth_token = ''
 my_num = '+16203158812'
 client = Client(account_sid, auth_token)
 
